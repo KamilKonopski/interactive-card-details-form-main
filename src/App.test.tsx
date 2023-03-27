e1faee8614cbcfabc9a8f631b30345c441c5072a
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-27 14:20:25 +0200

    remove

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-
-import App from "./App";
-
-it("should have a heading text 'Carts Dashboard'", () => {
-	render(<App />);
-	const headingText = screen.queryByText(/Carts Dashboard/i);
-	expect(headingText).toBeVisible();
-});
