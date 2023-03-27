d8d6b3aecc19e0b6311e3e617563dd4291f25ce7
Kamil Konopski <Kamil.Konopski.2000@gmail.com>
2023-03-27 14:27:34 +0200

    remove test

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	test: {
-		globals: true,
-		environment: "jsdom",
-		setupFiles: "./src/test/setup.ts",
-		css: true,
-	},
 });
